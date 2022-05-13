613c598678db5f9a9823d69744b2b5905b1d952e
Ziolkowski, Adam <Adam.Ziolkowski@sabre.com>
2022-05-13 17:58:08 -0500

    Add open-props stylesheets and postcss-preset-env

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { render } from 'solid-js/web'
 
+import './main.css'
 import App from './shell/App'
 
 const rootEl = document.getElementById('root')
--- src/shell/App.tsx
+++ src/shell/App.tsx
@@ -1,6 +1,8 @@
 import AppHeader from './AppHeader'
 import AppFooter from './AppFooter'
 
+import './app.css'
+
 const App = () => (
   <div class="app">
     <AppHeader />
@@ -10,6 +12,19 @@ const App = () => (
       </header>
       <div class="content">
         <p>There is nothing to see here yet.</p>
+
+        <div class="color-palette">
+          <div class="blue-0">Blue 0</div>
+          <div class="blue-1">Blue 1</div>
+          <div class="blue-2">Blue 2</div>
+          <div class="blue-3">Blue 3</div>
+          <div class="blue-4">Blue 4</div>
+          <div class="blue-5">Blue 5</div>
+          <div class="blue-6">Blue 6</div>
+          <div class="blue-7">Blue 7</div>
+          <div class="blue-8">Blue 8</div>
+          <div class="blue-9">Blue 9</div>
+        </div>
       </div>
     </main>
     <AppFooter />
